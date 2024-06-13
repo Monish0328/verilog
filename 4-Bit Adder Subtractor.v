09cca75dc8d528a0804ffca5c089a5f6ed4e1954
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-06-13 17:57:25 +0530

    Update 4-Bit Adder Subtractor.v

--- 4-Bit Adder Subtractor.v
+++ 4-Bit Adder Subtractor.v
@@ -4,9 +4,6 @@ module full_adder(
 );
   
   assign {sum, cout} = {a^b^cin, ((a & b) | (b & cin) | (a & cin))};
-  //or
-  //assign sum = a^b^cin;
-  //assign cout = (a & b) | (b & cin) | (a & cin);
 endmodule
 
 module ripple_carry_adder_subtractor #(parameter SIZE = 4) (
@@ -19,7 +16,7 @@ module ripple_carry_adder_subtractor #(parameter SIZE = 4) (
   assign Bc[0] = B[0] ^ CTRL;
     
   full_adder fa0(A[0], Bc[0], CTRL, S[0], Cout[0]);
-  generate  // This will instantial full_adder SIZE-1 times
+  generate  
     for(g = 1; g<SIZE; g++) begin
       assign Bc[g] = B[g] ^ CTRL;
       full_adder fa(A[g], Bc[g], Cout[g-1], S[g], Cout[g]);
