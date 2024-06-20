b1fb631b366b868d86f2be8d03b3b5c0480dfcea
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-06-20 19:17:10 +0530

    Update SISO.v

--- SISO.v
+++ SISO.v
@@ -22,17 +22,14 @@ module testbench();
 reg d,clk;
 wire q;
 siso a(d, clk, q);
-initial
-begin
+initial begin
 $monitor($time,"d=%b,clk=%b,q=%b",d,clk,q);
 end
-initial
-begin
+initial begin
 clk=1'b0;
 forever #5 clk=~clk;
 end
-initial
-begin
+initial begin
 d=1;
 #10 d=0;
 #10 d=1;
