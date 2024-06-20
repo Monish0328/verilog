b29bac6291a93f266ee670f21a539b74db30731f
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-06-20 19:15:58 +0530

    Update PISO.v

--- PISO.v
+++ PISO.v
@@ -22,21 +22,18 @@ module pisotb();
   wire q;
   piso dut(in,ld,clk,rst,q);
   
-   initial 
-    begin
+   initial begin
       clk = 1;
       forever #5 clk = ~clk;
     end
   
- initial 
-    begin
+ initial begin
       $dumpfile("pisotb.vcd");
       $dumpvars(0,pisotb);
       #100 $finish;
     end
   
-  initial 
-    begin
+  initial begin
       rst=1;
       #10 rst=0;ld=1;in=4'b0111;
       #10 ld=0;in=0;
