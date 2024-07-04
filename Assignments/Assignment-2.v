1b2b8310649d8f6c68984b4f5bd48c9c696bb44d
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-04 13:30:18 +0530

    Update Assignment-2.v

--- Assignments/Assignment-2.v
+++ Assignments/Assignment-2.v
@@ -1,17 +1,18 @@
 1. What is the value of y in binary format in the below snippet?
 
    reg [5:0] y;
-   initial
-   begin
-   y= &#39;Ox2;
+   initial begin
+    y='Ox2;
    end
+  A:0010
 
 2. What is the value of the b in the below snippet?
 
    integer b;
    initial begin
-     b= -&#39;d12/3;
+     b= -'d12/3;
    end
+   A: b= -4
 
 3. What value is passed across the port b in the below snippet?
 
@@ -20,6 +21,8 @@
     assign b = a;
    endmodule
 
+  A: b=14.5
+
 4. Find the value of the variable t at 1ns &amp; 20ns respectively from the
 below snippet:
   time = t;
@@ -27,10 +30,26 @@ below snippet:
    #10 t= $time;
    #20 ;
   end
+ A: t=10ns
 
 
 5. Write an RTL snippet to initialize all the locations of an array of size:10x8 to
 0 at 0ns. Depth= 10, Width = 8bits:
+
+  A:
+    module initialize_array;
+    reg [7:0] array [0:9][0:7];
+    initial begin
+    for (int i = 0; i < 10; i++) begin
+      for (int j = 0; j < 8; j++) begin
+        array[i][j] = 8'b0; 
+      end
+      end
+     end
+    initial begin
+      $finish;
+    end
+    endmodule
 
 
 6. What will be the value of the parameter constant for the instance DUT in
@@ -42,6 +61,9 @@ the top module from the below snippet?
   test #(16) OUT ( );
  endmodule
 
+   A:  in module test parameter is 8,
+      during instantiation it is overridden by 16.
+
 7. What is the bug in the following snippet?
    module test;
      parameter WIDTH=8;
@@ -52,6 +74,8 @@ the top module from the below snippet?
      defparam OUT.WIDTH=10;
    endmodule
 
+      A: parameters should be set explicitly at module instantiation rather than using defparam statements inside procedural blocks like 'initial'.
+
 
   8. What will be displayed in the console mode for the below snippet?
 
@@ -60,3 +84,5 @@ the top module from the below snippet?
       s = &quot;hello world&quot;;
       $display(&quot;Value = %s&quot;,s[104:65]);
     end
+    
+      A:
