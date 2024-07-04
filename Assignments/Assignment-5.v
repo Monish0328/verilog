e7f0da586c32049fb10d334a1af375de50a8cedb
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-04 15:32:54 +0530

    Update Assignment-5.v

--- Assignments/Assignment-5.v
+++ Assignments/Assignment-5.v
@@ -9,6 +9,8 @@
     y3 = y2;
    end
 
+  A:one d-flipflop.
+
 2. How many DFFs will be inferred from the below snippet:
 
    input c,din;
@@ -21,9 +23,26 @@
     y3 &lt;= y2;
    end
 
+   A:three d-flipflops.
+
 3. Write RTL code to design a 3-bit SISO right shift register using
 only a Non-blocking assignment. (Do not use any operators).
 
+      A:module SISO_ShiftRegister (
+        input wire clk,  
+        input wire reset, 
+        input wire si,   
+        output reg [2:0] q );
+   
+        always @(posedge clk or posedge reset) begin
+        if (reset) begin
+            q <= 3'b0;
+        end else begin
+            q <= {si, q[2], q[1]};
+        end
+        end
+
+       endmodule
 
 4. Which of the statements will execute from the below
 snippet &amp; justify the behavior?
@@ -36,6 +55,7 @@ snippet &amp; justify the behavior?
       3’b001 : st3;
     endcase
    end
+   A:
 
 5. Which hardware logic is inferred from the below snippet?
 
@@ -46,14 +66,41 @@ snippet &amp; justify the behavior?
       a[1] : y = 1;
       a[0] : y = 0;
     endcase
-  end
+  end 
+
+   A:a priority encoder or a priority decoder.
 
 6. Write RTL code for designing an 8:1 mux using a for loop.
+   A:module mux8_1 (
+     input [7:0] data_in, 
+     input [2:0] s,       
+     output reg [7:0] y );
 
+     always @* begin
+        y = 0; // Initialize output
+        for (int i = 0; i < 8; i = i + 1) begin
+            if (s == i) begin
+                y = data_in[i]; 
+            end
+            end
+     end
+     endmodule
   
 7. Write Verilog code for generating an infinite clock signal
 with a 50% Duty cycle having a time period of 10ns using a
 forever loop.
+    A:module InfiniteClock;
+      reg clk;  
+      always begin
+        #5 clk = ~clk; 
+      end
+      initial begin
+        clk = 0; 
+        forever begin
+            #5 clk = ~clk;  
+        end
+      end
+      endmodule
 
   
 8. Draw the waveform for the below snippet.
@@ -67,9 +114,26 @@ forever loop.
     rst =0;
    end
 
+    A:
+
+
 9. Write RTL code for designing a D latch using a 2:1 mux.
 
-  
+   A:module DLatch (
+     input D,    
+     input LE,   
+     output reg Q );
+     reg Q_bar;  
+     always @* begin
+        if (LE) begin
+            Q = D;      
+            Q_bar = ~D; 
+        end else begin
+            Q = Q;      
+            Q_bar = Q_bar; 
+        end
+     end
+     endmodule
 10. Find the output for the below codes:
 
   1)    reg a,b;
@@ -80,6 +144,12 @@ forever loop.
          #5 b &lt;= a;
          $display($time,&quot;a=%d,b=%d&quot;,a,b);
         end
+
+        A:#0 a=1, b=1
+          #10 a=0, b=1
+          #11 a=1, b=1
+          #16 a=1, b=1
+
   2)   reg a,b;
        always@(a,b) begin
          #10 a = 1&#39;b0;
@@ -88,6 +158,13 @@ forever loop.
          #5 b = a;
          $display($time,&quot;a=%d,b=%d&quot;,a,b);
        end
+
+          A:#0 a=1, b=1
+            #10 a=0, b=1
+            #11 a=1, b=1
+            #16 a=1, b=1
+
+
  3)    reg a,b;
        initial begin
         a=1;
@@ -99,6 +176,9 @@ forever loop.
         b&lt;=a;
         $display($time,&quot;a=%d,b=%d&quot;,a,b);
        end
+
+
+        A:#1 a=1, b=1
 
 11. For the below RTL schematic write the RTL snippet
 
