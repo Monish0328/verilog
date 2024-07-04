1d8ddee39cad8ce7f99dd722ab624f4956c07f3d
Monish0328 <143273825+Monish0328@users.noreply.github.com>
2024-07-04 11:21:15 +0530

    Update Assignment-1.v

--- Assignments/Assignment-1.v
+++ Assignments/Assignment-1.v
@@ -99,6 +99,22 @@ endmodule
 pclk_250mhz, rate[1:0]; output pclk, When Rate=&#39;b00 Select 62.5MHZ
 Clock; Rate=&#39;b10 Select 250MHZ Clock:
 
+  module clock_mux(
+    input wire pclk_62_5mhz,
+    input wire pclk_250mhz,
+    input wire [1:0] rate,
+    output reg pclk );
+
+   always @(*) begin
+     case(rate)
+        2'b00: pclk = pclk_62_5mhz;
+        2'b10: pclk = pclk_250mhz;
+        default: pclk = 1'b0; // Default case to avoid latches
+     endcase
+   end
+
+ endmodule
+
 
 
 9. Design a negative edge-triggered D flip flop with an active low reset:
